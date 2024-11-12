04e2e3f74453d6a834884a625ce04e14e765922f
Aaron-James-26 <aaronjamesdelacruz2004ii@gmail.com>
2024-11-12 15:09:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ const logger = (req, res, next) => {
 app.use(logger)
 //connection to mysql
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "Employee",
+    host: "bdti5cvzwmhgbnrxgqgt-mysql.services.clever-cloud.com",
+    user: "uuvkuar8mzp5htdj",
+    password: "wRHtCWHPOHPIlJMvayjF",
+    database: "bdti5cvzwmhgbnrxgqgt",
 });
 
 
